541e624de948dad508c8e618e1431962e3295cc9
simonbesteiro <simonchuelo@gmail.com>
2020-06-23 18:38:55 +0200

    add rankings

--- pasapalabra/index.js
+++ pasapalabra/index.js
@@ -9,12 +9,18 @@ Tu juego debería hacer una pregunta por cada letra del alfabeto, al final del j
 /*FALTA
 Rankings
 */
+var ranking = [
+    ["Maria",25],
+    ["John",22]
+];
+
 function pasapalabra() {
     var questions = [
         //it contains the info at the folowing order, first the question, secondly the answer, last the state which change when the user answer the question
         ["pregunta1","RESPUESTA1","NONE"],
         ["pregunta2","RESPUESTA2","NONE"],
     ];
+     var userName = prompt("Which will be your username?");
     do{
         for (var i = 0; i < questions.length; i++) {
             /*IDEA: el array questions tiene dentro otros array con la pregunta y e esado acertada, fallada o ninguna cuando hacemos apsapaabra quedara en ninguna y se recorera de nuevo
@@ -34,7 +40,11 @@ function pasapalabra() {
             }
         }
     }while(checkQuestions(questions))
+    rankingPlacement(questions,userName);
     console.log("Your final score is, "+counter(questions,"PASS")+" passed and "+counter(questions, "FAIL")+" failed.")
+    for (let j = 0; j < ranking.length; j++) {
+        console.log(ranking[i][0]+" has "+ranking[i][1]+" points \n");            
+    }
 }
 //Checks if there is a question without an user answer
 function checkQuestions(questions) {
@@ -46,7 +56,17 @@ function checkQuestions(questions) {
     }
     return checker;
 }
-
+function rankingPlacement(questions,userName){
+    var score=counter(questions,"PASS");
+    for (var i = 0; i < ranking.length; i++) {
+        var a= ranking[i]
+        if(ranking[i][1] < score){
+            ranking.splice(i,0,[userName, score]);
+            return;
+        }
+    }
+    ranking.push([userName, score])
+}
 // Counts the number of passed or failed questions
 function counter(questions,status) {
     var counter=0;
