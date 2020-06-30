012ab2aaf13d41060b0523696b8767f002106140
simonbesteiro <simonchuelo@gmail.com>
2020-06-30 12:09:26 +0200

    calculator-html creation v1 and v2

---
+++ calculator-HTMLv2/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @calculator-HTML
+ * @simonbesteiro Simón Fernández Besteiro <simonbesteiro@gmail.com>
+ *
+ * Created at: 2020-06-30 
+ */
+var display = document.getElementsByClassName('display')
+display.
+
+
+
