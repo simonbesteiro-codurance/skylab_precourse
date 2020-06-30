189fa7a8023b37ac0839f726f9fa557ca25650d3
simonbesteiro <simonchuelo@gmail.com>
2020-06-30 19:41:13 +0200

    calculator html w no js adapted

---
+++ calculator-HTML-wNoJS-Simon/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @calculator-HTML
+ * @simonbesteiro Simón Fernández Besteiro <simonbesteiro@gmail.com>
+ *
+ * Created at: 2020-06-30 
+ */
+//var display = document.getElementsByClassName('display')
+//display.innerHTML
+document.getElementsByClassName("display").innerHTML="TEST"
+
+
+
